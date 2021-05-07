5a9edafc1ec9be2875ab0be658d6e7599a870f92
NYD <nyd117@hotmail.com>
2021-05-07 22:22:53 +0300

    Added C# one liner.

--- C#/FizzBuzz/FizzBuzz/FizzBuzz.cs
+++ C#/FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 // Using Top-Level statements feature in c# 9.
 
@@ -11,3 +13,6 @@ for(int i = 1; i<=100; i++)
 }
 
 
+// Single statement with string interpolation
+
+Enumerable.Range(1, 100).ToList().ForEach(n => Console.WriteLine($"{($"{(n % 3 == 0 ? "Fizz" : null)}" + $"{(n % 5 == 0 ? "Buzz" : null)}" is { } res && res  != string.Empty ? res : n)}"));
